fa43a6a294cc4f28d7b2065977318369a1ebfd20
DinarBes <dinar.mukhamadeev.03@bk.ru>
2024-01-09 13:28:54 +0300

    new version ktor in build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
+        kotlinCompilerExtensionVersion = "1.5.7"
     }
     packaging {
         resources {
@@ -63,6 +63,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation(project(mapOf("path" to ":oneentry")))
+    testImplementation("org.testng:testng:6.9.6")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- oneentry/build.gradle.kts
+++ oneentry/build.gradle.kts
@@ -1,30 +1,33 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
-    id("org.jetbrains.kotlin.plugin.serialization")
-    id("maven-publish")
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.21"
+    application
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
+group = "org.example"
+version = "1.0-SNAPSHOT"
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+val ktor_version: String by project
 
 dependencies {
-
-    implementation("io.ktor:ktor-client-core:2.3.3")
-    implementation("io.ktor:ktor-client-cio:2.3.3")
-    implementation("io.ktor:ktor-serialization-gson:2.3.3")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.3")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.3")
-    implementation("io.ktor:ktor-client-auth:2.3.3")
-    implementation("io.ktor:ktor-server-auth-jwt:2.3.3")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0")
+    implementation("io.ktor:ktor-client-cio-jvm:2.3.7")
 
     implementation("org.jsoup:jsoup:1.16.1")
-//    testImplementation("junit:junit:4.13.2")
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+
+    testImplementation(kotlin("test"))
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
